e082e557cc1278396d666034e546774b1cc97515
Kilima Glenn <thunderenlight@gmail.com>
2023-02-18 20:56:13 +0000

    Adding docker layers

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -34,7 +34,7 @@ class HomeActivities:
     {
       'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
       'handle':  'Garek',
-      'message': 'My dear doctor, I am just simple tailor',
+      'message': 'My dear doctor, I am just simple fisher',
       'created_at': (now - timedelta(hours=1)).isoformat(),
       'expires_at': (now + timedelta(hours=12)).isoformat(),
       'likes': 0,
